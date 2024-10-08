4e4be52c68ba384d6991e1d5c5a6d8e31cf2f937
cicciord <102993057+cicciord@users.noreply.github.com>
2024-10-08 16:26:23 +0200

    set remote hardhat url

--- app-settings.ts
+++ app-settings.ts
@@ -2,7 +2,7 @@ import { hardhat } from "viem/chains";
 
 export const appSettings = {
   [hardhat.id]: {
-    rpcUrl: "http://192.168.1.131:8545",
+    rpcUrl: "https://radica.dev:8545",
     tagContractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     propertyContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
   },
